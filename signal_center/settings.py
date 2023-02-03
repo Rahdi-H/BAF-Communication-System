88338e1be8532ca6d842cfdd57e814e8457bf906
Rahdin Hussain <60464006+Rahdi-H@users.noreply.github.com>
2023-02-03 15:46:45 +0600

    Update settings.py

--- signal_center/settings.py
+++ signal_center/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n9(eij-k68x2r9c!w449l0kk38ez0nokvcd_(7bc8$0%6q#$c8'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
